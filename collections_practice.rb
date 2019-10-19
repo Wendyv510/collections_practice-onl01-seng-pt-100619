60544e519716b59aeed71a37a4a1a73fb952508c
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 17:45:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,12 +51,16 @@ def sort_array_char_count(length)
  reverse_array(12,4,35)
  
  def kesha_maker(names)
+   names -[] 
    kesha_maker.each do |names|
      {|name| $<<.third} 
    end 
  end 
  
  kesha_maker("Blake", "Ashley", "Scott")
+ 
+ 
+ def find_a
  
  
    

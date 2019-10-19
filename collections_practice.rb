8b867c63fa348fcb9743a90e2df5646c27be6bfe
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 17:16:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,22 +1,26 @@
 require_relative '../collections_practice_spec.rb'
 
 def sort_array_asc(ascending)
-  ascending =[25,7,1]
+  ascending =[]
   ascending.sort 
 end 
 
+sort_array(25,7,1)
+
 def sort_array_desc(descending)
-  descending = [25,7,14]
+  descending = []
  descending.sort do |b,a|
   b<=>a 
 end 
 end 
 
+sort_array_desc(25,7,14)
+
 def sort_array_char_count(length)
-  length = ["dog","cat","horses"]
+  length = []
   length.sort do {|a,b|.length}
      a <=> b 
    end 
  end 
  
- 
+ sort_array_char_count("dogs", "cat", "Horses")

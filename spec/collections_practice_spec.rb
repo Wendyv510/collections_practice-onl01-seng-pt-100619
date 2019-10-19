89f50b226da7f1801c36a1f53fd5433914a5915a
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 17:34:34 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -35,6 +35,9 @@ describe "swap_elements_from_to" do
   it "swap_elements_from_to" do 
     expect (swap_elements_from_to(["a","b","c"],0,2)).to eq(["C", "b", "a"])
     expect (swap_elements_from_to(["a","b","c"],2,1))to eq(["a", "c","b"])
+  end 
+end 
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do

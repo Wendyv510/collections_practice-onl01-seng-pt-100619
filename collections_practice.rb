c1478efe5998b97e1f3f2fac4d015f7576b83b11
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 17:10:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(ascending)
+  ascending =[]
+  ascending.sort 
+end 
 
+def sort_array_desc(descending)
+  descending = []
+ descending.sort do |b,a|
+  b<=>a 
+end 
+end 
+
+def sort_array_char_count(length)
+  length = []
+  length.sort do {|a,b|.length}
+     a <=> b 
+   end 
+ end 
+ 
+ 
